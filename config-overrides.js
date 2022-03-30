7bd14fb0d46f37ba79258d5e48878901aec928a6
Step <digiumbri@gmail.com>
2022-03-30 19:58:01 -0400

    updated logs

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,15 @@
 module.exports = function override (config, env) {
+    const fallback = config.resolve.fallback || {};
     console.log('override')
-    let loaders = config.resolve
-    loaders.fallback = {
+    // let loaders = config.resolve
+    // loaders.fallback = 
+    Object.assign(fallback, {
         "assert": require.resolve("assert"),
         "fs": false,
         "tls": false,
         "net": false,
         "http": require.resolve("stream-http"),
-        "https": false,
+        "https": require.resolve("https-browserify"),
         "zlib": false,
         "path": false,
         "stream": require.resolve("stream-browserify"),
@@ -15,7 +17,11 @@ module.exports = function override (config, env) {
         "url": require.resolve("url"),
         "os": require.resolve("os-browserify/browser"),
         "crypto": require.resolve("crypto-browserify")
-    }
+    })
+    
+    config.ignoreWarnings = [/Failed to parse source map/]
+
+    config.resolve.fallback = fallback;
     
     return config
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './polyfill'
 import {BrowserRouter} from 'react-router-dom'
 import ScrollToTop from './ScrollToTop';
 

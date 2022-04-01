fd96a957da56fae4a7831d62da4bbb2d9ddede19
Step <digiumbri@gmail.com>
2022-03-31 23:51:03 -0400

    Testing changed the App.js

---
+++ src/pages/newmenus.jsx
@@ -0,0 +1,19 @@
+const newmenus = [
+    {
+        id: 1,
+        name: 'Home',
+        links: '/',
+    },
+    {
+        id: 2,
+        name: 'Explore',
+        links: '/gacha',
+    },{
+        id: 3,
+        name: 'Activity',
+        links: '/faq',
+    },
+    
+]
+
+export default newmenus;

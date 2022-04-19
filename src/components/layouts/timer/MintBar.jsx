86e6fd6fe0dc605e4f88bfe3b9f899ea4c2b2490
Step <digiumbri@gmail.com>
2022-04-19 13:53:14 -0400

    Update MintBar.jsx

--- src/components/layouts/timer/MintBar.jsx
+++ src/components/layouts/timer/MintBar.jsx
@@ -8,7 +8,7 @@ const MintBar = () => {
         <ProgressBar
         percent={50}
         // percent={prices.remainingNft / 10000 * 100}
-        filledBackground="rgba(255, 204, 255, 0.6)"
+        filledBackground="rgba(249, 157, 204, 0.6)"
         height="30px"
         // text={"hello"} 
         

c51ddf495dc482e0d1124d3254ab3d9ec8aef37a
Kserisemo <74157194+Kserisemo@users.noreply.github.com>
2022-11-19 18:03:40 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ import GachaDetails from "./GachaDetails";
 // import SignUp from "./SignUp";
 // import NoResult from "./NoResult";
 import FAQ from "./FAQ";
-import Mint from "./Mint";
+//import Mint from "./Mint";
 // import Contact01 from "./Contact01";
 // import Contact02 from "./Contact02";
 
@@ -40,7 +40,7 @@ const routes = [
   { path: '/', component: <Home />},
   // { path: '/gacha', component: <Gacha/>},
   // { path: '/gachapon', component: <GachaDetails/>},
-  { path: '/mint', component: <Mint />},
+  // { path: '/mint', component: <Mint />},
   { path: '/faq', component: <FAQ />},
 ]
 
